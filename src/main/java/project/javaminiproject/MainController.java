6e4e28337ceb96eaae798acb77677f70283c9d35
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-11-19 19:59:14 +0530

    Added register fxml

---
+++ src/main/java/project/javaminiproject/MainController.java
@@ -0,0 +1,62 @@
+package project.javaminiproject;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class MainController {
+    private Stage stage;
+    private Scene scene;
+    private FXMLLoader fxmlLoader;
+
+
+    //login control functions
+    public void switchToMainApp(ActionEvent event){
+
+    }
+    public void switchToRegister(ActionEvent event){
+
+    }
+
+
+
+
+
+
+
+
+    //register control functions
+
+
+
+
+
+
+    //main page control functions
+    public void switchToEventPage(ActionEvent event){
+
+    }
+
+
+
+
+
+    //EventPage control functions
+    public void switchToMainPage(ActionEvent event){
+
+    }
+
+
+
+
+//    @FXML
+//    private Label welcomeText;
+//
+//    @FXML
+//    protected void onHelloButtonClick() {
+//        welcomeText.setText("Welcome to JavaFX Application!");
+//    }
+}
--- src/main/java/project/javaminiproject/mainpagecontroller.java
+++
@@ -1,4 +0,0 @@
-package project.javaminiproject;
-
-public class mainpagecontroller {
-}
