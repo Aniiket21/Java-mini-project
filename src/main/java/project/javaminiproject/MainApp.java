31b9c30c03b7a642f412d469ee13f83ee23216dc
Shubham Talawadekar <shubhamtalawadekar2002@gmail.com>
2021-11-20 10:44:38 +0530

    Added register fxml

--- src/main/java/project/javaminiproject/MainApp.java
+++ src/main/java/project/javaminiproject/MainApp.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("mainpage.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Event Management App");
         stage.setScene(scene);
---
+++ src/main/java/project/javaminiproject/module.java
@@ -0,0 +1,4 @@
+package project.javaminiproject;
+
+public class module {
+}
