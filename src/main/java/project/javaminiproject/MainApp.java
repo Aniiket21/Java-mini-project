d07735f23e6085ea8b939e00fc51fa63ddae4398
DishantWakalkar <dishantwakalkar@gmail.com>
2021-11-20 19:07:29 +0530

    mainapp

--- src/main/java/project/javaminiproject/MainApp.java
+++ src/main/java/project/javaminiproject/MainApp.java
@@ -2,21 +2,23 @@ package project.javaminiproject;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.Objects;
 
 public class MainApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApp.class.getResource("login.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Event Management App");
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("login.fxml")));
+        Scene scene = new Scene(root);
+        stage.setTitle("Event Management");
         stage.setScene(scene);
-        stage.setResizable(false);
         stage.show();
     }
+
 
     public static void main(String[] args) {
         launch();
