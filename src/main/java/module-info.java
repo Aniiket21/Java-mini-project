48172ffaf1bb1c4cc1a1dc3a1c0a70afc73d151d
Shubham Talawadekar <89774595+0-shubham-0@users.noreply.github.com>
2021-11-23 10:38:30 +0530

    Revert "login"

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,8 @@
 module project.javaminiproject {
     requires javafx.controls;
     requires javafx.fxml;
-requires javafx.graphics;
-requires java.sql;
-opens sample;
+
+
     opens project.javaminiproject to javafx.fxml;
     exports project.javaminiproject;
 }
